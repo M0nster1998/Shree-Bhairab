a8da0f9e2a5c28a03a84e54f7006b14774ed0496
M0nster1998 <pritam.mhzsmail@gmail.com>
2024-11-30 13:20:53 +0545

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <img src="../Images/Band Logo.png" alt="Band Logo" width="50"></img>
+            <img src="../Images/Band-Logo.png" alt="Band Logo" width="50"></img>
             <span className="ml-2 text-xl font-bold">Shree Bhairab</span>
           </div>
 
